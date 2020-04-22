c28b16881ff110cf287e22720bf08d9988250bd0
SrinivasaChakka <Srinivasa.Chakka@tesco.com>
2020-04-22 05:32:50 +0530

    CustomQueryImplementationinJPA repository
    adding custom queries in spring data jpa repositories

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -4,6 +4,10 @@ import com.guitar.db.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
+
+    List<Location> findByStateLike(String stateName);
 }
